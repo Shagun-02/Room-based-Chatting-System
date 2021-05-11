daf0e00fae3a8dd130e2258e7c72f550d0e0189d
m-triple-m <triplem656@gmail.com>
2021-05-11 10:47:36 +0530

    ..

--- firstapi/index.js
+++ firstapi/index.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(cors());
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-      }
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
 });
 
 const port = api_config.port; //specifying port 
@@ -20,38 +20,38 @@ const rooms = [];
 
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+  console.log('a user connected');
+
+  socket.emit('room_update', rooms);
+
+  socket.on('sendmsg', data => {
+    console.log(`message for room ${data.room}`);
+    // socket.broadcast.emit('rcvmsg', data);
+    socket.to(data.room).emit('rcvmsg', data);
+  })
+
+
+  socket.on('createroom', function (obj) {
+    socket.join(obj.room_name);
+    rooms.push(obj);
 
     socket.emit('room_update', rooms);
+    socket.broadcast.emit('room_update', rooms);
 
-    socket.on('sendmsg', data => {
-        console.log(`message for room ${data.room}`);
-        // socket.broadcast.emit('rcvmsg', data);
-        socket.to(data.room).emit('rcvmsg', data);
-    })
-
-
-    socket.on('createroom', function (obj) {
-        socket.join(obj.room_name);
-        rooms.push(obj);
-
-        socket.emit('room_update', rooms);
-        socket.broadcast.emit('room_update', rooms);
-
-        console.log(`room created with name ${obj.room_name}`)
-      });
-
-      socket.on('joinroom', function (name) {
-        socket.join(name);
-
-        console.log(`room joined with name ${name}`)
-      });
+    console.log(`room created with name ${obj.room_name}`)
   });
+
+  socket.on('joinroom', function (name) {
+    socket.join(name);
+
+    console.log(`room joined with name ${name}`)
+  });
+});
 
 //creating server
 http.listen(port, () => {
-    console.log('sever started at '+port);
+  console.log('sever started at ' + port);
 });
- 
+
 
 
